ab95e388e072bfa98d1575ebea9a4e0b216519a2
Lukasz Romerowicz <lukasz.romerowicz@stockport.gov.uk>
2019-05-29 14:19:37 +0100

    fix(Build)

--- tests/Controller/ValuesControllerTests.cs
+++ tests/Controller/ValuesControllerTests.cs
@@ -1,6 +1,8 @@
 using System;
 using fostering_service.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using StockportGovUK.AspNetCore.Availability.Managers;
 using Xunit;
 
 namespace fostering_service_tests.Controllers
@@ -8,10 +10,11 @@ namespace fostering_service_tests.Controllers
     public class ValuesControllerTests
     {
         private readonly ValuesController _valuesController;
+        private readonly Mock<AvailabilityManager> _availabilityManagerMock = new Mock<AvailabilityManager>();
 
         public ValuesControllerTests()
         {
-            _valuesController = new ValuesController();
+            _valuesController = new ValuesController(_availabilityManagerMock.Object);
         }
 
         [Fact]
