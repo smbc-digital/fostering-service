f4e0be703fc819b2e0e7f553213c8157d7c406f8
Lukasz Romerowicz <lukasz.romerowicz@stockport.gov.uk>
2019-05-29 14:43:31 +0100

    fix(Build): Fix default secrets file name

--- src/OperationalToggles.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace fostering_service
-{
-    public static class OperationalToggles
-    {
-        public const string MyToggle = "MyToggle";
-    }
-}
--- src/Startup.cs
+++ src/Startup.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -28,6 +29,19 @@ namespace fostering_service
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "fostering_service API", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "Authorization using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[] { }},
+                });
             });
 
             services.AddHttpClient();
