3b4918e80ed2b98cd37a1857bc351e7c76eb358d
Elliott Humphreys <elliott.humphreys@stockport.gov.uk>
2019-07-24 15:38:24 +0100

    test(BlockUpdateAttribute): add tests for new attribute WIP

---
+++ tests/Attributes/BlockFormUpdateAttributeTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using fostering_service.Services;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Moq;
+using Xunit;
+
+namespace fostering_service.Attributes
+{
+    public class BlockFormUpdateAttributeTests
+    {
+        private readonly BlockFormUpdateAttribute _attribute = new BlockFormUpdateAttribute();
+        private readonly Mock<IFosteringService> _mockFosteringService = new Mock<IFosteringService>();
+
+        public BlockFormUpdateAttributeTests()
+        {
+            //_mockHttpContext
+            //    .Setup(_ => _.ActionArguments)
+            //    .Returns(new Dictionary<string, object> {
+            //        {
+            //            "model", new TestingModel()
+            //        }
+            //    });
+
+            var mockRequestService = new Mock<IServiceProvider>();
+
+            mockRequestService
+                .Setup(_ => _.GetService(typeof(IFosteringService)))
+                .Returns(_mockFosteringService);
+
+
+        }
+
+        [Fact(Skip = "WIP")]
+        public void OnActionExecuting_ShouldCallFosteringService()
+        {
+            // Arrange
+
+
+            // Act
+            //_attribute.OnActionExecuting(_mockHttpContext.Object);
+
+            // Assert
+        }
+    }
+
+    internal class TestingModel
+    {
+        public string CaseReference { get; set; } = "12345678";
+    }
+}
