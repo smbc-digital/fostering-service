4063e9a8a87b17a84f246b0ec09919819a30f2e9
Elliott Humphreys <elliott.humphreys@stockport.gov.uk>
2019-09-11 13:54:05 +0100

    refactor(Program): make appsettings requried

--- src/Program.cs
+++ src/Program.cs
@@ -15,9 +15,9 @@ namespace fostering_service
         public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("./Config/appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"./Config/appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddJsonFile($"./Config/appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? string.Empty}.json", optional: false)
             .AddJsonFile("./Config/Secrets/appsettings.secrets.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"./Config/Secrets/appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.secrets.json", optional: true)
+            .AddJsonFile($"./Config/Secrets/appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? string.Empty}.secrets.json", optional: false)
             .AddEnvironmentVariables()
             .Build();
 
